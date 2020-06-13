501a61f67ad22134cca9d0bb5e030ab1794a8280
Artsiom Kaliaha <stakkato95@gmail.com>
2020-06-13 10:07:57 +0200

    Add simple socket example

---
+++ WebSocketTest/cmake-build-debug/CMakeFiles/WebSocketTest.dir/DependInfo.cmake
@@ -0,0 +1,35 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/artsiom/Documents/repository/SPROS/WebSocketTest/main.cpp" "/home/artsiom/Documents/repository/SPROS/WebSocketTest/cmake-build-debug/CMakeFiles/WebSocketTest.dir/main.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "POCO_ENABLE_CPP11"
+  "POCO_ENABLE_CPP14"
+  "POCO_HAVE_FD_EPOLL"
+  "POCO_OS_FAMILY_UNIX"
+  "_DEBUG"
+  "_FILE_OFFSET_BITS=64"
+  "_LARGEFILE64_SOURCE"
+  "_REENTRANT"
+  "_THREAD_SAFE"
+  "_XOPEN_SOURCE=500"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/usr/local/include/Poco"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
