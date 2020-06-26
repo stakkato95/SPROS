e57e9ab3ee0d3e4e13f75f1d339aeb7d48c74f5e
Artsiom Kaliaha <stakkato95@gmail.com>
2020-06-26 15:13:27 +0200

    Add simple mqtt client

--- MQTTClient/main.cpp
+++ MQTTClient/main.cpp
@@ -1,6 +1,47 @@
 #include <iostream>
+#include <string>
+#include <cstring>
+#include "mqtt/async_client.h"
 
-int main() {
-    std::cout << "Hello, World!" << std::endl;
+using namespace std;
+
+/////////////////////////////////////////////////////////////////////////////
+
+int main(int argc, char* argv[])
+{
+    const int QOS = 1;
+    const char* payload = "my msg";
+    string address = "tcp://localhost:1883";
+
+    cout << "Initializing for server '" << address << "'..." << endl;
+    mqtt::async_client cli(address, "");
+
+    cout << "  ...OK" << endl;
+
+    try {
+        cout << "\nConnecting..." << endl;
+        cli.connect()->wait();
+        cout << "  ...OK" << endl;
+
+        cout << "\nPublishing messages..." << endl;
+
+        mqtt::topic top(cli, "test", QOS);
+        mqtt::token_ptr tok;
+
+        size_t i = 0;
+        tok = top.publish(payload);
+        tok->wait();	// Just wait for the last one to complete.
+        cout << "OK" << endl;
+
+        // Disconnect
+        cout << "\nDisconnecting..." << endl;
+        cli.disconnect()->wait();
+        cout << "  ...OK" << endl;
+    }
+    catch (const mqtt::exception& exc) {
+        cerr << exc.what() << endl;
+        return 1;
+    }
+
     return 0;
 }
